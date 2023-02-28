b29954f5d4d9842d1e3accd2a83ca660d20cc90b
German Caicedo <german.caicedoa1@gmail.com>
2023-02-27 22:50:10 -0500

    Sweetalert added join whit name and just message

--- public/chat.js
+++ public/chat.js
@@ -1,30 +1,48 @@
 const socket = io();
 // DOM Elements
 let message = document.querySelector('#message');
-let username = document.querySelector('#username');
 let btn = document.querySelector('#send');
 let output = document.querySelector('#output');
 let actions = document.querySelector('#actions');
 
+//sweet alert
+Swal.fire({
+  icon: "info",
+  title: "Hi you are conected to German chat! c:",
+  text: "type your username",
+  input: "text",
+  inputValidator: (value)=>{
+    if (!value){
+      return "You must add a user"
+    }
+  },
+  allowOutsideClick: false
+}).then(result =>{
+  user = result.value
+  console.log(user)
+});
+
+
 btn.addEventListener('click', () => {
   socket.emit('client:message', {
-    username: username.value,
+    username: user,
     message: message.value
   });
 });
 
-message.addEventListener('keypress',()=>{
+message.addEventListener('keypress', () => {
   socket.emit('client:typing', username.value)
-})
+});
 
 socket.on('client:message', (data) => {
+  console.log(data);
   output.innerHTML += `<p>
   <strong>${data.username}</strong> : ${data.message}
   </p>`
-})
+});
 
-socket.on('client:typing', (data)=>{
+socket.on('client:typing', (data) => {
   actions.innerHTML = `<p>
   <em>${data} is typing...</em>
   </p>`
-})
+});
